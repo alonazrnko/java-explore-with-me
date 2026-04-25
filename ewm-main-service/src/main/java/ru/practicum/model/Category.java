c8d579229ceb9d07a3bc4ab852b48ce268538ac7
alonazrnko <alonazrnko@gmail.com>
2026-04-25 21:26:21 +0300

    feat(main): add schema.sql and User, Category entities

---
+++ ewm-main-service/src/main/java/ru/practicum/model/Category.java
@@ -0,0 +1,31 @@
+package ru.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
