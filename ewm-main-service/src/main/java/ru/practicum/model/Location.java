7bcb2a6a78fa673ff3e6baefea2f5159a19849e5
alonazrnko <alonazrnko@gmail.com>
2026-04-25 22:18:13 +0300

    feat(main): add Event and Location entities, add EventState enum

---
+++ ewm-main-service/src/main/java/ru/practicum/model/Location.java
@@ -0,0 +1,29 @@
+package ru.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "locations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Float lat;
+
+    @Column(nullable = false)
+    private Float lon;
+}
