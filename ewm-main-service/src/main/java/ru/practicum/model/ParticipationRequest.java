26bd7084297bcc0adb33efcaf44f21d126c783f6
alonazrnko <alonazrnko@gmail.com>
2026-04-26 22:00:53 +0300

    feat(main): add ParticipationRequest entity and RequestStatus enum

---
+++ ewm-main-service/src/main/java/ru/practicum/model/ParticipationRequest.java
@@ -0,0 +1,40 @@
+package ru.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.model.enums.RequestStatus;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "requests")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ParticipationRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requester_id", nullable = false)
+    private User requester;
+
+    @Column(nullable = false)
+    private LocalDateTime created;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private RequestStatus status;
+}
