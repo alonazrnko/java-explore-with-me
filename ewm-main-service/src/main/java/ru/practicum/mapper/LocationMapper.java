21a3ab3e5c06f2f7639c88bff03ad70ad768d224
alonazrnko <alonazrnko@gmail.com>
2026-04-26 21:39:00 +0300

    feat(main): add Location mapper

---
+++ ewm-main-service/src/main/java/ru/practicum/mapper/LocationMapper.java
@@ -0,0 +1,29 @@
+package ru.practicum.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.dto.LocationDto;
+import ru.practicum.model.Location;
+
+@Component
+public class LocationMapper {
+
+    public Location toLocation(LocationDto dto) {
+        if (dto == null) {
+            return null;
+        }
+        return Location.builder()
+                .lat(dto.getLat())
+                .lon(dto.getLon())
+                .build();
+    }
+
+    public LocationDto toLocationDto(Location location) {
+        if (location == null) {
+            return null;
+        }
+        return LocationDto.builder()
+                .lat(location.getLat())
+                .lon(location.getLon())
+                .build();
+    }
+}
