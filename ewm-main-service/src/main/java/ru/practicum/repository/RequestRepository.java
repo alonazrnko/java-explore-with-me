b62ce4af7ae044e4e5b4764b500aacfc02a72093
alonazrnko <alonazrnko@gmail.com>
2026-04-26 22:01:28 +0300

    feat(main): add request DTO and repository with count methods

---
+++ ewm-main-service/src/main/java/ru/practicum/repository/RequestRepository.java
@@ -0,0 +1,18 @@
+package ru.practicum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.model.ParticipationRequest;
+import ru.practicum.model.enums.RequestStatus;
+
+import java.util.List;
+
+public interface RequestRepository extends JpaRepository<ParticipationRequest, Long> {
+
+    Integer countByEventIdAndStatus(Long eventId, RequestStatus status);
+
+    List<ParticipationRequest> findAllByRequesterId(Long requesterId);
+
+    List<ParticipationRequest> findAllByEventId(Long eventId);
+    
+    Boolean existsByRequesterIdAndEventId(Long requesterId, Long eventId);
+}
