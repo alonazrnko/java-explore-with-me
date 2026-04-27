1cfd050cd64850047e961e82e5191d46256ba4b6
alonazrnko <alonazrnko@gmail.com>
2026-04-27 15:54:06 +0300

    refactor(stats): enforce strict typing in client and add builder to DTOs

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.practicum.dto.EndpointHitDto;
+import ru.practicum.dto.ViewStatsDto;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -26,11 +27,11 @@ public class StatsClient {
         this.serverUrl = serverUrl;
     }
 
-    public ResponseEntity<Object> saveHit(EndpointHitDto hitDto) {
-        return rest.postForEntity(serverUrl + "/hit", hitDto, Object.class);
+    public void saveHit(EndpointHitDto hitDto) {
+        rest.postForEntity(serverUrl + "/hit", hitDto, Void.class);
     }
 
-    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+    public ResponseEntity<ViewStatsDto[]> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
         Map<String, Object> parameters = new HashMap<>(Map.of(
                 "start", start.format(FORMATTER),
                 "end", end.format(FORMATTER),
@@ -40,10 +41,10 @@ public class StatsClient {
         StringBuilder pathBuilder = new StringBuilder(serverUrl + "/stats?start={start}&end={end}&unique={unique}");
 
         if (uris != null && !uris.isEmpty()) {
-            pathBuilder.append("&uris={uris}");
             parameters.put("uris", String.join(",", uris));
+            pathBuilder.append("&uris={uris}");
         }
 
-        return rest.getForEntity(pathBuilder.toString(), Object.class, parameters);
+        return rest.getForEntity(pathBuilder.toString(), ViewStatsDto[].class, parameters);
     }
 }
