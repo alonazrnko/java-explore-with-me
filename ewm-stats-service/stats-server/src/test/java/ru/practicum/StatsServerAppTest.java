f5372ce87ff54b868b0436354cc640182fa9e5ba
alonazrnko <alonazrnko@gmail.com>
2026-04-28 16:46:44 +0300

    test: add smoke test for StatsServer application context

---
+++ ewm-stats-service/stats-server/src/test/java/ru/practicum/StatsServerAppTest.java
@@ -0,0 +1,17 @@
+package ru.practicum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsServerAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void main() {
+        StatsServerApp.main(new String[] {});
+    }
+}
