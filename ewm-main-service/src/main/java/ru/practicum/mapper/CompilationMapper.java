b04d5546a72a4e5bf6573df624e3a87a973ec5f9
alonazrnko <alonazrnko@gmail.com>
2026-04-28 11:37:53 +0300

    test: secure CompilationMapper against null and add unit tests

--- ewm-main-service/src/main/java/ru/practicum/mapper/CompilationMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/mapper/CompilationMapper.java
@@ -11,11 +11,15 @@ import java.util.List;
 public class CompilationMapper {
 
     public CompilationDto toDto(Compilation compilation, List<EventShortDto> events) {
+        if (compilation == null) {
+            return null;
+        }
+
         return CompilationDto.builder()
                 .id(compilation.getId())
                 .pinned(compilation.getPinned())
                 .title(compilation.getTitle())
-                .events(events)
+                .events(events) 
                 .build();
     }
 }
