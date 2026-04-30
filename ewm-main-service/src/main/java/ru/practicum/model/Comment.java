303415faf60f67348312d1824a863d4213b1fc74
alonazrnko <alonazrnko@gmail.com>
2026-04-30 14:54:22 +0300

    feat: add Comment entity and CommentState enum

---
+++ ewm-main-service/src/main/java/ru/practicum/model/Comment.java
@@ -0,0 +1,39 @@
+package ru.practicum.model;
+
+import lombok.*;
+import ru.practicum.model.enums.CommentState;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2000)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column(nullable = false)
+    private LocalDateTime created;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private CommentState state;
+}
